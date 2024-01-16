1f0dc4c3a86e3e12368831d696626dfd74d5382f
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-01-16 23:05:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const APIURL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+
+const movie_box = document.querySelector('#movies-container');
+
+const getMovies = async(api) => {
+    const response = await fetch(api)
+    const data = await response.json()
+    console.log(data)
+    showMovies(data.results)
+}
+
+
+const showMovies = (data) => {
+
+    movie_box.innerHTML = "";
+    data.forEach(item => {
+        const box = document.createElement("div");
+        box.classList.add("box")
+        box.innerHTML = `
+        <img src="${IMGPATH + item.poster_path}" alt="poster">
+                <div class="info">
+                    <div class="title">
+                        <h2>${item.original_title}</h2>
+                        <span>${item.vote_average}</span>
+                    </div>
+
+                    <h3>Overview: </h3>
+                    <p>${item.overview}</p>
+                </div>
+        `; 
+        movie_box.appendChild(box)
+    })
+}
+
+document.querySelector('#search').addEventListener("keyup", (e) => {
+    if (e.target.value != null) {
+        getMovies(SEARCHAPI + e.target.value);
+    } else {
+        getMovies(APIURL);
+    }
+})
+// init call
+getMovies(APIURL);
+
