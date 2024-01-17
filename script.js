08b2d762362de55208fe6204fafbda804cacec48
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-01-17 11:21:17 +0530

    layout change

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-const APIURL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const APIURL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+const SEARCHAPI =
+  'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
-const movie_box = document.querySelector('#movies-container');
+const movie_box = document.querySelector('#movie-container');
 
-const getMovies = async(api) => {
-    const response = await fetch(api)
-    const data = await response.json()
-    console.log(data)
-    showMovies(data.results)
-}
-
+const getMovies = async (api) => {
+  const response = await fetch(api);
+  const data = await response.json();
+  console.log(data);
+  showMovies(data.results);
+};
 
 const showMovies = (data) => {
+  movie_box.innerHTML = '';
+  data.forEach((item) => {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.innerHTML = `
+               <img src="${IMGPATH + item.poster_path}"
+                   alt="movie poster" />
+                  <div class="info">
+                      <h2>${item.original_title}</h2>
+                      <span><i class='bx bxs-star'></i>${
+                        item.vote_average
+                      }</span>
+                  </div>
+          `;
 
-    movie_box.innerHTML = "";
-    data.forEach(item => {
-        const box = document.createElement("div");
-        box.classList.add("box")
-        box.innerHTML = `
-        <img src="${IMGPATH + item.poster_path}" alt="poster">
-                <div class="info">
-                    <div class="title">
-                        <h2>${item.original_title}</h2>
-                        <span>${item.vote_average}</span>
-                    </div>
+    movie_box.appendChild(box);
+  });
+};
 
-                    <h3>Overview: </h3>
-                    <p>${item.overview}</p>
-                </div>
-        `; 
-        movie_box.appendChild(box)
-    })
-}
-
-document.querySelector('#search').addEventListener("keyup", (e) => {
-    if (e.target.value != null) {
-        getMovies(SEARCHAPI + e.target.value);
-    } else {
-        getMovies(APIURL);
-    }
-})
+document.querySelector('#search').addEventListener('keyup', (e) => {
+  if (e.target.value != null) {
+    getMovies(SEARCHAPI + e.target.value);
+  } else {
+    getMovies(APIURL);
+  }
+});
 // init call
 getMovies(APIURL);
-
